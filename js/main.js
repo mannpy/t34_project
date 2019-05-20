2ad5be952b48c762bc4d4a73394c74445932be7e
mannpy <maaz1313@gmail.com>
2019-05-20 17:20:15 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// $(function(){
+
+// 	GeoPortal.on("ready",function() {
+
+//         mapObject = new GeoPortal.Map('map');
+    
+//         GeoPortal.findLayerById (
+//             207,
+//             function(layer){
+//                 console.log('layer: ', layer);
+//                 mapObject.addLayer(layer);
+//             },
+//             function(status,error){
+//                 console.log(error);
+//             });
+//         },this);
+
+//     });
+
+// //lat=55.29006455319217&lon=36.617431640625&zoom=8&baselayer=-932516122&layers=207&outformat=frame"></iframe>
