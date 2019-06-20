4493285ac12dedb27d9003e5a49a348a53612df2
mannpy <maaz1313@gmail.com>
2019-06-20 19:56:48 +0500

    Characteristics changes

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
     var history = $('.history-carousel');
 
     history.owlCarousel({
-        items:2,
+        items:3,
         loop:false,
         center:false,
         margin:80,
